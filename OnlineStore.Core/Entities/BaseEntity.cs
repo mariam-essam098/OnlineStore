a24120d27886918f6b21555a53465581b8474ca7
Mariam Essam <mariamessam098@yahoo.com>
2024-10-04 20:56:03 +0300

    02 Product Module -Entities

---
+++ OnlineStore.Core/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Entities
+{
+	public class BaseEntity<Tkey>
+	{
+		//Foriegn key for Brand 
+		public Tkey Id { get; set; }
+
+		public DateTime CreateAt { get; set; }=DateTime.UtcNow;
+
+
+
+
+
+
+
+
+	}
+}
---
+++ OnlineStore.Core/Entities/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Entities
+{
+	public class Product :BaseEntity<int>
+	{
+      
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string PictureUrl { get; set; }
+
+
+        public  decimal Price { get; set; }
+
+        //Foriegn key for Brand 
+        public int? BrandId { get; set; }
+
+        //Navigational property
+
+        public ProductBrand Brand { get; set; }
+
+		//Foriegn key for type 
+		public int? TypeId { get; set; }
+
+		public ProductType Type { get; set; }
+
+
+
+    }
+}
